cebf8b8453eb7623f577f6878f1949c7e79ab30c
Michele Brodoloni <miklinux@users.noreply.github.com>
2021-06-25 09:10:52 +0200

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -4,6 +4,7 @@ sudo mdutil -i off -a
 # configure docker
 brew install docker docker-machine
 brew install --cask virtualbox
+mkdir -p ~/.docker/machine/cache
 curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v18.09.1-rc1/boot2docker.iso
 docker-machine create --driver virtualbox default
 eval "$(docker-machine env default)"
