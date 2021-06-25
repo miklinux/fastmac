555883bac47c28da45dabdeb5639baac9860977a
Michele Brodoloni <miklinux@users.noreply.github.com>
2021-06-25 09:08:03 +0200

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,9 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
-echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
+
+# configure docker
+brew install docker docker-machine
+brew install --cask virtualbox
+curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v18.09.1-rc1/boot2docker.iso
+docker-machine create --driver virtualbox default
+eval "$(docker-machine env default)"
